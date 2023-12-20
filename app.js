1caa63ceb46e205ef4c72834a031b475cdc3c7d8
char26 <charliealders26@gmail.com>
2023-12-20 15:55:13 -0700

    express server taking screenshots

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const { unlinkSync, existsSync, mkdirSync } = require('fs');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+if (!existsSync("/tmp/screenshots")) {
+    mkdirSync("/tmp/screenshots");
+}
+
+app.get('/screenshot', (req, res) => {
+    (async () => {
+        const url = req.query["url"];
+        const width = req.query["width"] || 1920;
+        const height = req.query["height"] || 1080;
+
+        const uid = Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9 * Math.pow(10, 12)).toString(36);
+        const filePath = `/tmp/screenshots/${uid}.jpg`;
+
+        const browser = await puppeteer.launch({
+            headless: "new"
+        });
+        const page = await browser.newPage();
+
+        await page.setViewport({ width: width, height: height });
+        try {
+            await page.goto(url);
+        } catch (e) {
+            res.status(400).send("Error in URL.");
+        }
+
+        try {
+            await page.screenshot({ path: filePath });
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        } finally {
+            await browser.close();
+            res.sendFile(filePath, () => {
+                unlinkSync(filePath);
+            });
+        }
+    })();
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on port ', PORT);
+});
